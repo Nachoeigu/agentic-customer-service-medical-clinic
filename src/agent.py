3ea43de92d8bc683ac153e3561c645ba180ddaf2
ignacio.eiguren@gmail.com <ignacio.eiguren@gmail.com>
2024-08-05 14:34:43 +0200

    .

--- src/agent.py
+++ src/agent.py
@@ -8,9 +8,10 @@ os.chdir(WORKDIR)
 sys.path.append(WORKDIR)
 
 from typing import Annotated, Literal, TypedDict
-
+import json
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
 from langchain_core.tools import tool
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import END, StateGraph, MessagesState
@@ -24,12 +25,7 @@ import logging_config
 
 logger = logging.getLogger(__name__)
 
-def loading_retriever(app):
-    app.loading_vdb(index_name = 'ovidedentalclinic')
-    retriever = app.vdb.as_retriever(search_type="similarity", 
-                                    search_kwargs={"k": 2})
-    
-    return retriever
+
 
 def format_retrieved_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
@@ -43,47 +39,150 @@ retriever = pinecone_conn.vdb.as_retriever(search_type="similarity",
 
 rag_chain = retriever | format_retrieved_docs
 
+
 class MessagesState(TypedDict):
     messages: Annotated[List[AnyMessage], operator.add]
 
-# Define the tools for the agent to use
+
+from langchain_core.pydantic_v1 import constr, BaseModel, Field, validator
+import re
+import pandas as pd
+
+class DateTimeModel(BaseModel):
+    """
+    The way the date should be structured and formatted
+    """
+    date: str = Field(..., description="Propertly formatted date", pattern=r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$')
+
+    @validator("date")
+    def check_format_date(cls, v):
+        if not re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$', v):
+            raise ValueError("Datetime must be in the format 'YYYY-MM-DD HH:MM'")
+        return v
+class DateModel(BaseModel):
+    """
+    The way the date should be structured and formatted
+    """
+    date: str = Field(..., description="Propertly formatted date", pattern=r'^\d{4}-\d{2}-\d{2}$')
+
+    @validator("date")
+    def check_format_date(cls, v):
+        if not re.match(r'^\d{4}-\d{2}-\d{2}$', v):
+            raise ValueError("Date must be in the format 'YYYY-MM-DD'")
+        return v
+
+    
+class IdentificationNumberModel(BaseModel):
+    """
+    The wat the DNI should be structured and formatted
+    """
+    dni: int = Field(..., description="identification number without dots", pattern=r'^\d{7,8}$')
+
+    @validator("dni")
+    def check_format_dni(cls, v):
+        if not re.match(r'^\d{7,8}$',str(v)):
+            raise ValueError("DNI must be in the format a integer value of 7 or 8 numbers without dots")
+        return v
+    
+
+
+
+#All the tools to consider
 @tool
-def check_availability(desired_date=False):
-    """Checking the database if the doctor has availability"""
+def check_availability(desired_date:DateModel, specialization:Literal["general_dentist", "cosmetic_dentist", "prosthodontist", "pediatric_dentist","emergency_dentist","oral_surgeon","orthodontist"]):
+    """
+    Checking the database if we have availability for the specific specialization.
+    """
+    desired_date = desired_date.date
+    #Dummy data
+    df = pd.read_csv(f"{WORKDIR}/data/syntetic_data/availability.csv")
+    rows = df[(df['date_slot'].apply(lambda x : x.split(' ')[0]) == desired_date)&(df['specialization'] == specialization)&(df['is_available'] == True)][['doctor_name','date_slot']]
+    logger.info(rows)
+    if len(rows) == 0:
+        output = "No availability in the entire day"
+    else:
+        output = '\n'.join(', '.join(f"{col}: {row[col]}" for col in rows.columns) for _, row in rows.iterrows())
+
+    return output
+
+@tool
+def reschedule_appointment(old_date:DateTimeModel, new_date:DateTimeModel, dni_number:IdentificationNumberModel, doctor_name:Literal['kevin anderson','robert martinez','susan davis','daniel miller','sarah wilson','michael green','lisa brown','jane smith','emily johnson','john doe']):
+    """
+    Rescheduling an appointment
+    """
     return True
 
 @tool
-def reschedule_appointment(old_date, new_date, dni_number):
-    """Rescheduling an appointment"""
+def cancel_appointment(date:DateTimeModel, dni_number:IdentificationNumberModel, doctor_name:Literal['kevin anderson','robert martinez','susan davis','daniel miller','sarah wilson','michael green','lisa brown','jane smith','emily johnson','john doe']):
+    """
+    Canceling an appointment
+    """
     return True
 
 @tool
-def set_appointment(date, dni_number):
-    """Set appointment with the doctor"""
+def get_catalog_specialists():
+    """
+    Obtain information about the doctors and specializations/services we provide
+    """
+    with open(f"{WORKDIR}/data/catalog.json","r") as file:
+        file = json.loads(file.read())
+    
+    return file
+
+@tool
+def set_appointment(desired_date:DateTimeModel, dni_number:IdentificationNumberModel, specialization:Literal["general_dentist", "cosmetic_dentist", "prosthodontist", "pediatric_dentist","emergency_dentist","oral_surgeon","orthodontist"]):
+    """
+    Set appointment with the doctor
+    """
     return True
 
 @tool
-def check_results(dni_number):
-    """Check if the result of the pacient is available"""
+def check_results(dni_number:IdentificationNumberModel):
+    """
+    Check if the result of the pacient is available
+    """
     return True
 
 @tool
-def reminder_appointment(dni_number):
-    """Returns when the pacient has its appointment with the doctor"""
+def reminder_appointment(dni_number:IdentificationNumberModel):
+    """
+    Returns when the pacient has its appointment with the doctor
+    """
     return "You have for next monday at 7 am"
 
 @tool
-def retrieve_faq_info(question):
-    """Retrieve documents from general questions about the medical clinic"""
+def retrieve_faq_info(question:str):
+    """
+    Retrieve documents from general questions about the medical clinic
+    """
     return rag_chain.invoke(question)
 
-tools = [retrieve_faq_info, set_appointment, reminder_appointment, check_availability, check_results,reschedule_appointment, reschedule_appointment]
+tools = [cancel_appointment, get_catalog_specialists, retrieve_faq_info, set_appointment, reminder_appointment, check_availability, check_results,reschedule_appointment, reschedule_appointment]
 
 tool_node = ToolNode(tools)
 
-model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+#model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+model = ChatGoogleGenerativeAI(model = 'gemini-1.5-pro-exp-0801', temperature = 0)
+#model = model.bind_tools(tools = tools)
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+class add(BaseModel):
+    """Add two integers."""
+
+    a: int = Field(..., description="First integer")
+    b: int = Field(..., description="Second integer")
+
+
+class multiply(BaseModel):
+    """Multiply two integers."""
+
+    a: int = Field(..., description="First integer")
+    b: int = Field(..., description="Second integer")
+tools = [add, multiply]
 model = model.bind_tools(tools)
-from langchain.prompts import ChatPromptTemplate
+
+x = model.invoke('2+2?')
 from datetime import datetime
 
 
@@ -98,24 +197,26 @@ def should_continue(state: MessagesState) -> Literal["tools", "human_feedback"]:
 def should_continue_with_feedback(state: MessagesState) -> Literal["agent", "end"]:
     messages = state['messages']
     last_message = messages[-1]
-    if isinstance(last_message, HumanMessage):
+    if (isinstance(last_message, HumanMessage))|(last_message.get("type","") == 'human'):
         return "agent"
     return "end"
 
 
 def call_model(state: MessagesState):
-    messages = state['messages']
-    
+    messages = [SystemMessage(content=f"You are helpful assistant in Ovide Clinic, dental care center in California (United States). As reference, this is the CURRENT TIME: {datetime.now().strftime('%Y-%m-%d %H:%M, %A')}. Keep a friendly, professional tone. Based your answers on the information from your tool calls.")] + state['messages']
     response = model.invoke(messages)
     return {"messages": [response]}
 
+#The commented part is because it breaks the UI with the input function
 def read_human_feedback(state: MessagesState):
-    if state['messages'][-1].tool_calls == []:
-        logger.info("AI: \n"+ state['messages'][-1].content)
-        user_msg = input("Reply: ")
-        return {'messages': [HumanMessage(content = user_msg)]}
-    else:
-        pass
+    # if state['messages'][-1].tool_calls == []:
+    #     logger.info("AI: \n"+ state['messages'][-1].content)
+    #     user_msg = input("Reply: ")
+    #     return {'messages': [HumanMessage(content = user_msg)]}
+    # else:
+    #     pass
+    pass
+
 
 workflow = StateGraph(MessagesState)
 
@@ -143,18 +244,17 @@ workflow.add_conditional_edges(
 
 
 workflow.add_edge("tools", 'agent')
-workflow.add_edge("agent", 'human_feedback')
 
 
 checkpointer = MemorySaver()
 
-app = workflow.compile(checkpointer=checkpointer)
+app = workflow.compile(checkpointer=checkpointer,
+                       interrupt_before=['human_feedback'])
 
 if __name__ == '__main__':
     final_state = app.invoke(
         {"messages": [
-            SystemMessage(content=f"You are helpful assistant in Ovide Clinic, dental care center in California (United States). Your job is to solve the query of this customer. As reference, the current hour is {datetime.now().strftime('%Y-%m-%d %H:%M')}"),
-            HumanMessage(content="Is it open tomorrow morning?")
+            HumanMessage(content="Do you have availability for tomorrow morning for dentist for my 5 years old kid?")
             ]},
         config={"configurable": {"thread_id": 42}}
     )
